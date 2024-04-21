45e6c08e11cb13c4cf57985251ba21173526c83d
Daniel Lamberti <daniellamberti@gmail.com>
2024-04-21 17:50:39 -0300

    first commit

---
+++ src/Producto.java
@@ -0,0 +1,10 @@
+public class Product {
+
+    String nombre;
+    double precio;
+
+    public Product(String nombre, double precio) {
+        this.nombre = "nombre";
+        this.price = precio;
+    }
+}
